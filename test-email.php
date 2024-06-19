2fff36e4b42c7f51d4128e09aa9a15efe7a8de5e
muhpuc40 <mdhassan49.muh@gmail.com>
2024-06-19 20:44:44 +0600

    updated -> Gitignore [username, password]

--- test-email.php
+++ test-email.php
@@ -1,36 +1,44 @@
 <?php
 include('smtp/PHPMailerAutoload.php');
 
+// Load configuration
+$config = include('config.php');
+
 $email = $_POST['email'];
 $sub = $_POST['sub'];
 $body = $_POST['msg'];
 
 echo smtp_mailer($email, $sub, $body);
-function smtp_mailer($to,$subject, $msg){
-	$mail = new PHPMailer(); 
-	$mail->IsSMTP(); 
-	$mail->SMTPAuth = true; 
-	$mail->SMTPSecure = 'tls'; 
-	$mail->Host = "smtp.gmail.com";
-	$mail->Port = 587; 
-	$mail->IsHTML(true);
-	$mail->CharSet = 'UTF-8';
-	//$mail->SMTPDebug = 2; 
-	$mail->Username = "mdhassan49.muh@gmail.com";
-	$mail->Password = "bogp uzhz ndvi ndch"; 
-	$mail->SetFrom("mdhassan49.muh@gmail.com");
-	$mail->Subject = $subject;
-	$mail->Body =$msg;
-	$mail->AddAddress($to);
-	$mail->SMTPOptions=array('ssl'=>array(
-		'verify_peer'=>false,
-		'verify_peer_name'=>false,
-		'allow_self_signed'=>false
-	));
-	if(!$mail->Send()){
-		echo $mail->ErrorInfo;
-	}else{
-		return 'Sent';
-	}
+
+function smtp_mailer($to, $subject, $msg){
+    global $config; // Access the config array
+
+    $mail = new PHPMailer(); 
+    $mail->IsSMTP(); 
+    $mail->SMTPAuth = true; 
+    $mail->SMTPSecure = 'tls'; 
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 587; 
+    $mail->IsHTML(true);
+    $mail->CharSet = 'UTF-8';
+    //$mail->SMTPDebug = 2; 
+
+    $mail->Username = $config['SMTP_USERNAME'];
+    $mail->Password = $config['SMTP_PASSWORD']; 
+    $mail->SetFrom($config['SMTP_USERNAME']);
+    $mail->Subject = $subject;
+    $mail->Body = $msg;
+    $mail->AddAddress($to);
+    $mail->SMTPOptions = array('ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => false
+    ));
+    
+    if(!$mail->Send()){
+        echo $mail->ErrorInfo;
+    } else {
+        return 'Sent';
+    }
 }
 ?>
